7cf73e010b6aad69792f7fcd7367c83029437224
Evelyn Kosma <ezranaja@gmail.com>
2021-02-28 19:49:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,10 +6,6 @@ def roll_call_dwarves(list)
 end
 
 def summon_captain_planet(array)
-<<<<<<< HEAD
-=======
- array = ["carrot", "cucumber", "pepper", "apple", "banana", "orange"]
->>>>>>> 18f52e590bab97904d2aed6e9e116fc960c2a1d4
  array.each {|x| x.capitalize! }
  array.collect {|array| array + "!"}
 end
@@ -22,6 +18,13 @@ def long_planeteer_calls(calls)
   end
 end
   
+
+def find_the_cheese(cheese)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese| 
+    cheese_types.include?(cheese)
+  end
+end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
